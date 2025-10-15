616ef0d0a0641f2d3d64160f26c6fa79fc2e59f2
n2jlaa <60931984+n2jlaa@users.noreply.github.com>
2025-10-15 14:35:33 +0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,12 +3,3 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-najla-devopsproj2"
-    storage_account_name = "najlaatfstateacct"
-    container_name       = "tfstate"
-    key                  = "devops2-najla/terraform.tfstate"
-    use_azuread_auth     = true
-  }
-}
