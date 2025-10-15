6122bba28040cf60eed8efc44d1039b0316e5bbc
n2jlaa <60931984+n2jlaa@users.noreply.github.com>
2025-10-15 14:53:42 +0300

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,24 +1,26 @@
 terraform {
   required_version = ">= 1.6.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.117"
-backend "azurerm" {
-  resource_group_name  = "rg-najla-devopsproj2"
-  storage_account_name = "najlaatfstateacct"
-  container_name       = "tfstate"
-  key                  = "terraform.tfstate"
-}
-
-
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-najla-devopsproj2"
+    storage_account_name = "najlaatfstateacct"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    # لو تبي تستخدمين Azure AD بدلاً من access key:
+    # use_azuread_auth = true
   }
 }
 
 provider "azurerm" {
-  features {
-
-  }
+  features {}
+  # تقدر تتركي الاشتراك بدون ما تكتبيه هنا وتستخدمي az login + az account set
+  # أو عبر متغير بيئة ARM_SUBSCRIPTION_ID
   subscription_id = "4421688c-0a8d-4588-8dd0-338c5271d0af"
 }
