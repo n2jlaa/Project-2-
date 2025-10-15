67d14199dfe24164800a6feec12e2427c40b8dff
n2jlaa <60931984+n2jlaa@users.noreply.github.com>
2025-10-15 15:16:56 +0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
- resource_group_name = data.azurerm_resource_group.rg.name
-location            = data.azurerm_resource_group.rg.location
+  name     = var.rg_name
+  location = var.location
 
 }
 
